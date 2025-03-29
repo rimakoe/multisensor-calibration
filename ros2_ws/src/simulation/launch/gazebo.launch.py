2b1a0832b640d0c40916d8668a3adca8b7ed98d1
rimakoe <richard.koeppe@outlook.de>
2025-03-29 15:28:47 +0000

    made generic world creation from xacro and camera and lidar completely generic from xacro and integrated ros gz bridge with rviz

--- ros2_ws/src/simulation/launch/gazebo.launch.py
+++ ros2_ws/src/simulation/launch/gazebo.launch.py
@@ -5,87 +5,122 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch.actions import IncludeLaunchDescription
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
+from launch.actions import (
+    IncludeLaunchDescription,
+    ExecuteProcess,
+    TimerAction,
+    RegisterEventHandler,
+    LogInfo,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import LaunchConfiguration, Command
+from launch.event_handlers import OnProcessExit
+
+camera_names = ["top_view_camera_front", "top_view_camera_rear", "top_view_camera_left", "top_view_camera_right"]
+lidar_names = ["ref_lidar"]
+
+
+def create_lidar_bridges(lidar_topic_namepsace: str = "lidar") -> list[str]:
+    lidar_bridges = []
+    for name in lidar_names:
+        prefix = "/" + "/".join([lidar_topic_namepsace, name])
+        topic_points = "/".join([prefix, "points"])
+        lidar_bridges.append("@".join([topic_points, "sensor_msgs/msg/PointCloud2", "gz.msgs.PointCloudPacked"]))
+    return lidar_bridges
+
+
+def create_camera_bridges(camera_topic_namepsace: str = "camera") -> list[str]:
+    camera_bridges = []
+    for name in camera_names:
+        prefix = "/" + "/".join([camera_topic_namepsace, name])
+        topic_info = "/".join([prefix, "camera_info"])
+        topic_image = "/".join([prefix, "image"])
+        camera_bridges.append("@".join([topic_info, "sensor_msgs/msg/CameraInfo", "gz.msgs.CameraInfo"]))
+        camera_bridges.append("@".join([topic_image, "sensor_msgs/msg/Image", "gz.msgs.Image"]))
+    return camera_bridges
+
 
 def generate_launch_description():
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name = 'simulation'  # <--- CHANGE ME
-
+    package_name = "simulation"
     base_path = Path(get_package_share_directory(package_name))
-    world_path = base_path / 'worlds'
-    xacro_file = os.path.join(base_path, 'urdf', 'robot.urdf')
+    path_to_robot_xacro = os.path.join(base_path, "urdf", "robot.urdf.xacro")
+    path_to_world_xacro = os.path.join(base_path, "worlds", "generic.urdf.xacro")
+    path_to_world_sdf = path_to_world_xacro.strip(".xacro").replace(".urdf", ".sdf")
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-        launch_arguments=[('gz_args', [f" -r -v 2 {world_path / 'empty.sdf'}"])]
+    launch_gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("ros_gz_sim"),
+                    "launch",
+                    "gz_sim.launch.py",
+                )
+            ]
+        ),
+        launch_arguments=[("gz_args", [f" -r -v 2 {path_to_world_sdf}"])],
     )
-    
-    path_to_urdf = os.path.join(get_package_share_directory(package_name), 'urdf', 'robot.urdf.xacro')
-    
+
+    # Generate the world sdf from its xacro
+    generate_sdf_from_xacro = ExecuteProcess(
+        cmd=["ros2", "run", "xacro", "xacro", path_to_world_xacro, ">", path_to_world_xacro.strip(".xacro").replace(".urdf", ".sdf")],
+        output="screen",
+        shell=True,
+        on_exit=LogInfo(msg="Successfully generated world sdf file from xacro. Starting Gazebo..."),
+    )
+
+    # Ensure Gazebo starts only after the Xacro conversion is done
+    launch_gazebo_after_xacro = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=generate_sdf_from_xacro,
+            on_exit=[launch_gazebo],
+        )
+    )
+
     # Create a robot_state_publisher node
     node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'robot_description': ParameterValue(Command(['xacro ', str(path_to_urdf)]), value_type=str)
-        }]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": ParameterValue(Command(["xacro ", str(path_to_robot_xacro)]), value_type=str)}],
     )
-    # Spawn robot directly from Xacro
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                                arguments=[
-                                    '-file', xacro_file,
-                                    '-name', 'vehicle',
-                                    '-x', '0.0',
-                                    '-y', '0.0',
-                                    '-z', '0.0',
-                                ],
-                                output='screen'
-                                )
 
     spawn_entity = Node(
         package="ros_gz_sim",
         executable="create",
-        arguments=[
-            "-name",
-            "robot1",
-            "-topic",
-            "/robot_description",
-            "-x",
-            "0",
-            "-y",
-            "0",
-            "-z",
-            "1.4",
-        ],
+        arguments=["-name", "vehicle", "-topic", "/robot_description", "-x", "0", "-y", "0", "-z", "0.0"],
         output="screen",
     )
 
-    spawn = ExecuteProcess(
-            cmd=['ros2', 'run', 'gazebo_ros', 'spawn_entity.py', 
-                 '-file', xacro_file, '-entity', 'my_robot'],
-            output='screen'
-        )
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+        ]
+        + create_camera_bridges()
+        + create_lidar_bridges(),
+        output="log",
+    )
+
+    rviz2 = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", os.path.join(base_path, "config", "standard.rviz")],
+    )
+
     # Launch them all!
-    return LaunchDescription([
-        gazebo,
-        node_robot_state_publisher,
-        #spawn_entity
-        TimerAction(
-            period=5.0,  # Delay in seconds
-            actions=[
-                spawn_entity
-            ]
-        ),
-    ])
-    
+    return LaunchDescription(
+        [
+            generate_sdf_from_xacro,
+            launch_gazebo_after_xacro,
+            bridge,
+            node_robot_state_publisher,
+            TimerAction(period=5.0, actions=[rviz2]),  # Delay in seconds
+            TimerAction(period=5.0, actions=[spawn_entity]),  # Delay in seconds
+        ]
+    )
--- ros2_ws/src/simulation/setup.py
+++ ros2_ws/src/simulation/setup.py
@@ -2,30 +2,30 @@ import os
 from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'simulation'
+package_name = "simulation"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.sdf'))),
-        (os.path.join('share', package_name, "models"), glob(os.path.join('models', '*.sdf'))),
-        (os.path.join('share', package_name, "urdf"), glob(os.path.join('urdf', '*.xacro')))
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "config"), glob(os.path.join("config", "*.rviz"))),
+        (os.path.join("share", package_name, "launch"), glob(os.path.join("launch", "*.launch.py"))),
+        (os.path.join("share", package_name, "worlds"), glob(os.path.join("worlds", "*.xacro"))),
+        (os.path.join("share", package_name, "worlds"), glob(os.path.join("worlds", "*.sdf"))),
+        (os.path.join("share", package_name, "models"), glob(os.path.join("models", "*.sdf"))),
+        (os.path.join("share", package_name, "urdf"), glob(os.path.join("urdf", "*.xacro"))),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="root",
+    maintainer_email="root@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
